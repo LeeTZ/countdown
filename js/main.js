eb5e26946038176813c2ede3ef529540d3797559
LeeTZ <litiezheng513@gmail.com>
2015-08-11 20:59:10 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,52 @@
+
+// here it's simple you have to just change your  date !!!
+$(document).ready(function() {
+	$("#count").countdown({
+		//to change date just replace the current date with yours .
+		date: "august 16, 2015",  
+		//html code in count div here.
+		htmlTemplate: "<div id='days-count' class='numbers'>%{d}<span class='days-label'>days</span></div> <div id='hours-count' class='numbers'>%{h}<span class='hours-label'>hours</span></div><div id='min-count' class='numbers'>%{m}<span class='min-label'>min</span></div><div id='sec-count' class='numbers'>%{s}<span class='sec-label'>sec</span></div>"
+	});
+});
+
+////////////////////show-hide tools panel////////////////////////
+$("#show-hide").live("click", function(){ 
+	if ($('#tools').is(':visible'))
+		{$(this).html('+');$("#tools").hide();}
+	else{$(this).html('-');$("#tools").show();}
+ }); 
+
+////////////////////skins selectors change////////////////////////
+$("#blue-skin").live("click", function(){ 
+	$("#container").attr("class","blue");
+ }); 
+
+$("#red-skin").live("click", function(){ 
+	$("#container").attr("class","red");
+ }); 
+
+$("#purple-skin").live("click", function(){ 
+	$("#container").attr("class","purple");
+ }); 
+
+$("#green-skin").live("click", function(){ 
+	$("#container").attr("class","green");
+ }); 
+
+
+////////////////////patterns selectors change////////////////////////
+$("#pat1").live("click", function(){ 
+	 $('html').attr("class","bg1");
+ }); 
+
+$("#pat2").live("click", function(){ 
+	$('html').attr("class","bg2");
+ }); 
+
+$("#pat3").live("click", function(){ 
+	$('html').attr("class","bg3");
+ }); 
+
+$("#pat4").live("click", function(){ 
+	$('html').attr("class","bg4");
+ }); 
